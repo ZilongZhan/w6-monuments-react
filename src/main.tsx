b6d392108376ffa4b8893f46ea6662bcdb8c468e
Zilong Zhan <zolong.zhan@gmail.com>
2025-04-07 21:10:39 +0200

    Add Header to app layout

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,10 @@ import { createRoot } from "react-dom/client";
 import App from "./components/App/App";
 
 import "./styles/styles.css";
+import "@fontsource-variable/outfit/index.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
-  </StrictMode>
+  </StrictMode>,
 );
