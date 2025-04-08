e5c0759f1d31ca53a2c52341c0c6fcfa993218ff
Zilong Zhan <zolong.zhan@gmail.com>
2025-04-08 21:30:44 +0200

    Add navigation component

---
+++ src/components/Navigation/Navigation.test.tsx
@@ -0,0 +1,19 @@
+import { MemoryRouter } from "react-router";
+import Navigation from "./Navigation";
+import { render, screen } from "@testing-library/react";
+
+describe("Given the Navigation component", () => {
+  describe("When it renders", () => {
+    test("Then it should show a 'Home' link", () => {
+      render(
+        <MemoryRouter>
+          <Navigation />
+        </MemoryRouter>,
+      );
+
+      const homeLink = screen.queryByRole("link", { name: /home/i });
+
+      expect(homeLink).toBeVisible();
+    });
+  });
+});
