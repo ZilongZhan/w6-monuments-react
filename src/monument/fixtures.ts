f5e6e774cbe0130966d4c8544fdc6fc8a2f5f04a
Zilong Zhan <zolong.zhan@gmail.com>
2025-04-09 23:13:39 +0200

    Add monument fixtures

---
+++ src/monument/fixtures.ts
@@ -0,0 +1,23 @@
+import { Monument } from "./types.js";
+
+const myNeighborMariasHouse: Monument = {
+  id: "1",
+  name: "Maria's house",
+  description: "An old house that is otherwise normal",
+  imageUrl: "http://mariashouse.com/house.webp",
+  imageAlt: "Iconic angle of Maria's house",
+  city: "Mariapolis",
+  country: "Marialand",
+};
+
+const myNeighborIgnaciosHouse: Monument = {
+  id: "2",
+  name: "Ignacio's house",
+  description: "An old house that is otherwise normal",
+  imageUrl: "A modern house that is otherwise normal",
+  imageAlt: "http://ignacioshouse.com/house.webp",
+  city: "Ignaciopolis",
+  country: "Ignaciolandia",
+};
+
+export { myNeighborIgnaciosHouse, myNeighborMariasHouse };
