269543c6847dce51bd5d088d7375f1167ea0b5bd
Zilong Zhan <zolong.zhan@gmail.com>
2025-04-09 23:52:39 +0200

    Define routes to MonumentsListPage

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,10 +1,15 @@
+import { Outlet } from "react-router";
 import Header from "../Header/Header";
+
 import "./App.css";
 
 const App: React.FC = () => {
   return (
     <div className="container">
       <Header />
+      <div className="main-content">
+        <Outlet />
+      </div>
     </div>
   );
 };
