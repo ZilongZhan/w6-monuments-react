2242589e57dbe5606b09c83e5e75c712432374c2
Zilong Zhan <zolong.zhan@gmail.com>
2025-04-10 19:43:03 +0200

    Add MonumentFormPage

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -6,9 +6,9 @@ const Header: React.FC = () => {
   return (
     <header className="main-header">
       <div className="title-container">
-        <img src="monument.svg" alt="Website logo" className="page-logo" />
-        <h1 className="page-title page-title--long">monuments.</h1>
-        <h1 className="page-title page-title--short">mnmnts.</h1>
+        <img src="monument.svg" alt="Website logo" className="app-logo" />
+        <h1 className="app-title app-title--long">monuments.</h1>
+        <h1 className="app-title app-title--short">mnmnts.</h1>
       </div>
       <Navigation />
     </header>
---
+++ src/monument/pages/MonumentFormPage/MonumentFormPage.tsx
@@ -0,0 +1,12 @@
+import MonumentForm from "../../components/MonumentForm/MonumentForm";
+
+const MonumentFormPage: React.FC = () => {
+  return (
+    <>
+      <h2 className="page-title">Add new monument</h2>
+      <MonumentForm />
+    </>
+  );
+};
+
+export default MonumentFormPage;
