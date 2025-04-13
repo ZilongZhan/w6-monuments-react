a60222cd4c05fad5d5847055c0d7f554c5ea9a6b
Zilong Zhan <zolong.zhan@gmail.com>
2025-04-13 22:45:02 +0200

    Add Footer component

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,11 @@
+import "./Footer.css";
+
+const Footer: React.FC = () => {
+  return (
+    <footer className="main-footer" aria-label="Website footer">
+      © 2025 Zilong Zhan. All rights reserved
+    </footer>
+  );
+};
+
+export default Footer;
