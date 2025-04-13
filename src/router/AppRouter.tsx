d254616959863e5c9511a328daee5d331ce82379
Zilong Zhan <zolong.zhan@gmail.com>
2025-04-13 22:11:44 +0200

    Add NotFoundPage

--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -2,6 +2,7 @@ import { Navigate, Route, Routes } from "react-router";
 import App from "../components/App/App";
 import MonumentsHomePage from "../monument/pages/MonumentsHomePage/MonumentsHomePage";
 import MonumentFormPage from "../monument/pages/MonumentFormPage/MonumentFormPage";
+import NotFoundPage from "../pages/NotFoundPage";
 
 const AppRouter: React.FC = () => {
   return (
@@ -10,6 +11,7 @@ const AppRouter: React.FC = () => {
         <Route index element={<Navigate to="/home" />} />
         <Route path="/home" element={<MonumentsHomePage />} />
         <Route path="/add-monument" element={<MonumentFormPage />} />
+        <Route path="*" element={<NotFoundPage />} />
       </Route>
     </Routes>
   );
