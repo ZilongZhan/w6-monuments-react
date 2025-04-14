500f1b3262a03c21a6e97b51bf5fa2082a4a8722
Zilong Zhan <zolong.zhan@gmail.com>
2025-04-14 02:40:05 +0200

    Define min-height for MonumentCardsList

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -11,7 +11,13 @@ const Header: React.FC<HeaderProps> = ({ handleSetShowSidebar }) => {
   return (
     <header className="main-header">
       <div className="title-container">
-        <img src="monument.svg" alt="Website logo" className="app-logo" />
+        <img
+          src="monument.svg"
+          alt="Website logo"
+          className="app-logo"
+          width="25"
+          height="25"
+        />
         <h1 className="app-title app-title--long">monuments.</h1>
         <h1 className="app-title app-title--short">mnmnts.</h1>
       </div>
